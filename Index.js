14bb7aeb92c19f8896870e07a04197ebb4a9d725
uelsodee <natthanbelamri1@gmail.com>
2025-08-03 14:08:02 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,17 +1,26 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config(); // Assure-toi que dotenv est installé !
+require('dotenv').config();  // Utilisation de dotenv pour gérer les variables d'environnement
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+// Création du client Discord
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ]
+});
 
+// Quand le bot est prêt
 client.once('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}`);
 });
 
+// Si un message est envoyé avec le contenu '+ping'
 client.on('messageCreate', message => {
     if (message.content === '+ping') {
         message.reply('Pong !');
     }
 });
 
-// Se connecter au bot avec le token de l'environnement
-client.login(process.env.TOKEN);
+// Connexion au bot avec la variable d'environnement
+client.login(process.env.DISCORD_TOKEN);  // Utilisation de la variable DISCORD_TOKEN
